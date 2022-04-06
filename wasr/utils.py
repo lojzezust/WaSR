c24e877a1561927368390aaf8a6c33d03a1a3275
Lojze Žust <lojze.zust@fri.uni-lj.si>
2022-04-06 11:12:24 +0000

    Removed code unecessary for inference

--- wasr/utils.py
+++ wasr/utils.py
@@ -1,25 +1,10 @@
 import os
 import torch
 from torch import nn
-import pytorch_lightning as pl
 
 from collections import OrderedDict
 from collections.abc import Iterable, Mapping
 from typing import Dict
-
-def load_weights(path):
-    state_dict = torch.load(path, map_location='cpu')
-    if 'model' in state_dict:
-        # Loading weights from checkpoint
-        state_dict = state_dict['model']
-
-    return state_dict
-
-class ModelExporter(pl.Callback):
-    """Exports model weights at the end of the training."""
-    def on_fit_end(self, trainer, pl_module):
-        export_path = os.path.join(trainer.log_dir, 'weights.pth')
-        torch.save(pl_module.model.state_dict(), export_path)
 
 class IntermediateLayerGetter(nn.ModuleDict):
     """
@@ -78,28 +63,4 @@ class IntermediateLayerGetter(nn.ModuleDict):
                 out_name = self.return_layers[name]
                 out[out_name] = x
         return out
-
-
-def tensor_map(obj, fn):
-    """Map a function to a nested tensor structure.
-    Example:
-    >>> fn = lambda t: t.to('cpu')
-    >>> a = tensor_map(a, fn)
-    """
-
-    if torch.is_tensor(obj):
-        return fn(obj)
-
-    elif isinstance(obj, Mapping):
-        dtype = type(obj)
-        res = ((k, tensor_map(v, fn)) for k, v in obj.items())
-        res = dtype(res)
-        return res
-    elif isinstance(obj, Iterable):
-        dtype = type(obj)
-        res = (tensor_map(v, fn) for v in obj)
-        res = dtype(res)
-        return res
-    else:
-        raise TypeError("Invalid type for tensor_map")
 
